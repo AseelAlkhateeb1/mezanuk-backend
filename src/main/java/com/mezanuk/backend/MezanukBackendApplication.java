a5ab3bc481eb41f42ae6eb00d74437f44e25fbaf
AseelAlkhateeb1 <aseel6981o@gmail.com>
2026-01-03 21:17:14 +0300

    Initial Spring Boot backend

---
+++ src/main/java/com/mezanuk/backend/MezanukBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mezanuk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MezanukBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MezanukBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mezanuk/backend/MezanukBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezanuk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MezanukBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
